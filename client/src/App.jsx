812fb6ba3bc7c2073799ec491f183b004d6b4124
Kalashsatyapal <142204147+Kalashsatyapal@users.noreply.github.com>
2025-08-14 10:17:02 +0530

    graph labels 3d

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,13 +11,15 @@ import Dashboard from "./pages/Dashboard";
 import AdminDashboard from "./components/AdminDashboard";
 import { AuthContext, AuthProvider } from "./context/AuthContext";
 import UploadHistory from "./components/UploadHistory";
-import DataVisualization from "./components/DataVisualization/DataVisualization";
+import DataVisualization from "./pages/DataVisualization/DataVisualization";
+
+
 
 function PrivateRoute({ children, roles }) {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
 
+  if (loading) return <div>Loading...</div>;
   if (!user) return <Navigate to="/login" />;
-
   if (roles && !roles.includes(user.role)) return <Navigate to="/" />;
 
   return children;
